a09a5d8020ced8c7a7f9d119f545cc9a3511b490
Lux <luxprajapati81@gmail.com>
2024-06-05 19:06:59 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import MainSection from "./pages/MainSection";
 
+import productData from "../src/asset/data";
+
 function App() {
+  console.log("Product Data", productData);
   return (
     <div className="App">
       <Navbar></Navbar>
       <MainSection />
+      <img src={productData[1].image2} alt="ima"></img>
     </div>
   );
 }
