d2cba1cfac141fa93500e11db435688af280bbf0
Lux <luxprajapati81@gmail.com>
2024-06-05 22:11:36 +0530

    About page Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Products from "./component/Products";
+import About from "./pages/About";
 import MainSection from "./pages/MainSection";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <MainSection />
       <Products />
+      <About />
     </div>
   );
 }
